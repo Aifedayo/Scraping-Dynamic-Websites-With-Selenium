23d4c6d0ac055e46f301204d5b3e2511eee6d104
Aifedayo <akeemifedayolag@gmail.com>
2024-06-02 10:00:00 +0100

    Use find_element

--- scraper.py
+++ scraper.py
@@ -3,13 +3,15 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 service = Service(executable_path="./chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.google.com")
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-input_element.send_keys("Data Science Jobs in Nigeria")
+input_element.send_keys("Data Science Jobs in Nigeria" + Keys.ENTER)
+
 
 time.sleep(10)
 
