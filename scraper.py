3b2713c69c11fb653b8b98b1fd0e0cd0904a65da
Aifedayo <akeemifedayolag@gmail.com>
2024-06-04 10:00:00 +0100

    Use find_element

--- scraper.py
+++ scraper.py
@@ -10,6 +10,7 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.google.com")
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear() # clear first before inputting text
 input_element.send_keys("Data Science Jobs in Nigeria" + Keys.ENTER)
 
 
