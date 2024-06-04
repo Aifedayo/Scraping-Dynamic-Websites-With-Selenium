63188cb07ac36e2e77c158e30467cf1df203eb10
Aifedayo <akeemifedayolag@gmail.com>
2024-06-04 10:00:00 +0100

    Use find_element

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC # Wait for an element to load before proceeding    
 
 service = Service(executable_path="./chromedriver")
 driver = webdriver.Chrome(service=service)
