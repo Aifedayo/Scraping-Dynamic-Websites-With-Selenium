d2b9b054fd4a697e4f23b2fd4782bac27136fc55
Aifedayo <akeemifedayolag@gmail.com>
2024-06-13 10:00:00 +0100

    Cookieclicker import...

--- cookieclicker.py
+++ cookieclicker.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -5,3 +7,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+# Instantiate the service
+service = Service(executable_path="./chromedriver")
